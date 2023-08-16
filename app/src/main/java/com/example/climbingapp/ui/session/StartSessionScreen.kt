b7e933e4e05960781b521ec2454c3c497783354c
Troy Tomlins <tpt38@uclive.ac.nz>
2023-08-16 20:47:56 +1200

    Set up a basic Start Session Screen

--- app/src/main/java/com/example/climbingapp/ui/session/StartSessionScreen.kt
+++ app/src/main/java/com/example/climbingapp/ui/session/StartSessionScreen.kt
@@ -1,22 +1,48 @@
 package com.example.climbingapp.ui.session
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.climbingapp.ui.theme.ClimbingAppTheme
 
 /**
  * Start new session screen
  */
 @Composable
-fun StartSessionScreen(modifier: Modifier = Modifier) {
-
+fun StartSessionScreen(onStart: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Current Location: ",
+            modifier = Modifier
+        )
+        Spacer(
+            modifier = Modifier.height(64.dp)
+        )
+        Button(onClick = onStart) {
+            Text(text = "Start New Session")
+        }
+    }
 }
 
 @Composable
 @Preview
 fun StartSessionPreview() {
     ClimbingAppTheme {
-        StartSessionScreen()
+        StartSessionScreen({},Modifier.background(Color.White))
     }
 }
